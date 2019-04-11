fe9f69407c8dd3578a72808a9bf4c71c30ccae67
TaylorSwinton <taylorswinton1@gmail.com>
2019-04-11 18:29:20 -0500

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,12 @@ sydney = User.create(name: "Sydney", email: "sydneyemail@gmail.com", password: "
 
 Home.create(home_address: "3821 Carabous Drive Birmingham, AL 18437", bedroom: "1", bathroom: "2",  price: "451,151.00", description: "Beautiful lake front home", user_id: taylor.id)
 
-Home.create(home_address: "3821 Carabous Drive Birmingham, AL 18437", bedroom: "1", bathroom: "2",  price: "451,151.00", description: "Beautiful lake front home", user_id: sydney.id)
+Home.create(home_address: "3821 killer ave, AL 18437", bedroom: "5", bathroom: "6",  price: "841,151.00", description: "Spacious ivy mansion", user_id: sydney.id)
+
+#OR YOU CAN DO IT THE WAY BELOW!!!
+#Use AR to pre-associate data:
+
+#howard.Home.create(home_address: "3821 Carabous Drive Birmingham, AL #18437", bedroom: "1", bathroom: "2",  price: "451,151.00", description: #"Beautiful lake front home")
+
+#sydneys_home = sydney.homes.build(home_address: "4513 Laughet Blvd Hoover, AL 4183", bedroom: "4", bathroom: "5",  price: "841,151.00", description: "Spacious ivy mansion")
+#sydneys_home.save #build doesn't automatically save to the db!
