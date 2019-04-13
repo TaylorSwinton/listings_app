039f8e62efa920dadd2b0b46da0cf6bb817747b2
TaylorSwinton <taylorswinton1@gmail.com>
2019-04-12 21:06:45 -0500

    finished up sign in and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,19 +23,42 @@ class UsersController < ApplicationController
         else
             # tell the user they entered invalid credentials
             # redirect them to the login page
-
         end
 
     end
 
     # what routes do i need for signup?
     get '/signup' do
+        erb :signup
+    end
 
+    post '/users' do
+        #here is where we will create a new user and persist the new user to the DB
+        # only want to persi
+        if params[:name] != "" && params[:email] != "" && params[:password] != "" && params[:phone_number] != "" && params[:base_address] != ""
+            # valid input
+            @user = User.create(params)
+            session[:user_id] = @user.id #actually logging the user in
+            redirect "/users/#{@user.id}"
+            #redirects to another route!
+        else
+            #not valid input
+            #it would be better to include a message - stretch goal!
+            redirect '/signup'
+        end
     end
 
     # user SHOW route
     get '/users/:id' do
-        "This will be the user show routes"
+        #Do this first
+        @user = User.find_by(id: params[:id])
+
+        erb :'/users/show'#erb sends you to a view
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/'
     end
 
 end
