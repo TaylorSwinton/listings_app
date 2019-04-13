0d9fa1e421497303db888a9e46f2fafecab4f512
TaylorSwinton <taylorswinton1@gmail.com>
2019-04-13 10:15:58 -0500

    update sign up and login, created home listings

--- app/controllers/home_listings_controller.rb
+++ app/controllers/home_listings_controller.rb
@@ -5,9 +5,26 @@ class HomeListingsController < ApplicationController
     end
     # post home to create a new journal entry
     post '/home_listings' do
-    
-    end
-    # show route for a journal entry
+        #create a new home listing and save it to the DB
+        #only save if is has some content
+        #only create if the user is logged in 
+        if !logged_in?
+            redirect '/'
+        end
+         #only save if the listing has content
+        if params[:home_address] != "" && params[:bedroom] != "" && params[:bathroom] != "" && params[:price] != "" && params[:description] != ""
+            #create a new entry
+            @home_listing = Home.create(home_address: params[:home_address], bedroom: params[:bedroom], bathroom: params[:bathroom], price: params[:price], description: params[:description], user_id: current_user.id)
 
-    # index route for all journal entries
+            redirect "/home_listings/#{@home_listing.id}"
+        else
+            redirect '/home_listings/new'
+        end
+    end
+    # show route for a home listing
+    get '/home_listings/' do
+
+    end
+
+    # index route for all home listings
 end
